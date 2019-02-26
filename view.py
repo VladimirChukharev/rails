04ef76885cb924529600b8a27a19689249ed07f8
Vladimir Chukharev <Vladimir.Chukharev@gmail.com>
2019-02-27 00:01:28 +0200

    Show 4 scaled random test images with labels.

--- view.py
+++ view.py
@@ -4,6 +4,8 @@
 """Show images and labels for it."""
 
 # from PIL import Image, ImageTk
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -36,22 +38,61 @@ def show_images(images, cols=1, titles=None):
     plt.show()
 
 
+def get_labels(images_dir, filename):
+    """Read labels from file"""
+    labels = np.loadtxt(os.path.join(images_dir, filename)).reshape((-1, 5, 2))
+    return labels
+
+
+def shuffle(length, indexes, images, labels1, labels2):
+    """Randomly shuffle indexes and return and length elements selected by indexes from each list"""
+    np.random.shuffle(indexes)
+    indxs = indexes[:length]
+    return (
+        indxs,
+        (images[ind] for ind in indxs),
+        (labels1[ind] for ind in indxs),
+        (labels2[ind] for ind in indxs),
+    )
+
 def main():
     """Do the job"""
+    show_images_number = 4
+    scaling_factor = 1/5
     images_dir = ("d:\\chukh\\Documents\\Texts\\Tyohaku\\2019\\ReconAI\\Recruitment_test\\"
                   "public-20190225T121409Z-001\\public\\test\\")
     images_ext = ".png"
-    nums = np.array(np.arange(1, 301))  # Images are named as NN.png, where NN = 1...300
+    green = (0, 196, 0)
+    red = (196, 0, 0)
+    blue = (0, 0, 196)
+    nums = np.array(np.arange(300))  # Images are named as NN.png, where NN = 1...300
     left_labels_name = "test_labels_left.txt"
     right_labels_name = "test_labels_right.txt"
-    print(images_dir, images_ext)
-    print(nums)
+
+    left_labels = get_labels(images_dir, left_labels_name)
+    right_labels = get_labels(images_dir, right_labels_name)
+    # print(nums)
     np.random.shuffle(nums)
-    names = ["{}{}".format(num, images_ext) for num in nums[:3]]
+    nums = nums[:show_images_number]
+    names = ["{}{}".format(num+1, images_ext) for num in nums]
     full_names = ["{}{}".format(images_dir, name) for name in names]
+    left_labels = np.array([(x, y) for (x, y) in [left_labels[num][pnt] for num in nums for pnt in range(5)]])
+    left_labels = left_labels.reshape((-1, 5, 2)) * scaling_factor
+    right_labels = np.array([(x, y) for (x, y) in [right_labels[num][pnt] for num in nums for pnt in range(5)]])
+    right_labels = right_labels.reshape((-1, 5, 2)) * scaling_factor
     print(names)
     print(full_names)
-    images = [cv2.resize(cv2.imread(name), (200, 320)) for name in full_names]
+    print(left_labels)
+    print(left_labels[0])
+    images = [cv2.resize(cv2.imread(name), None, None, scaling_factor, scaling_factor) for name in full_names]
+    for ind in range(len(images)):
+        polyline_l = left_labels[ind].astype(np.int32).reshape((-1, 2))
+        polyline_r = right_labels[ind].astype(np.int32).reshape((-1, 2))
+        print(ind, nums[ind], polyline_l)
+        # Draw red line segments 2 px wide through 5 points
+        cv2.polylines(images[ind], [polyline_l], False, red, 2)
+        cv2.polylines(images[ind], [polyline_r], False, red, 2)
+        # cv2.line(images[ind], (0, 0), (50, 50), (196, 0, 0), 1)
     show_images(images, titles=names)
 
 
